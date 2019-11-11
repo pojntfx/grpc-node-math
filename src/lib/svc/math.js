647c71b1368df5f1ce60ce208e278ef8492bbdc5
Felix Pojtinger <felix@pojtinger.com>
2019-11-11 19:51:06 +0100

    feat: Use a prepublish script

--- src/lib/svc/math.js
+++ src/lib/svc/math.js
@@ -1,53 +1,57 @@
-const {MathAddReply} = require('../../../.generated/src/lib/proto/math_pb');
+const { MathAddReply } = require("../../../.generated/math_pb");
 
 /**
  * Add two numbers
  */
 module.exports.add = (call, callback) => {
-  // Validate input
-  if (call.request.getFirst() === 0) {
-    console.error('could not add, `First` has not been provided');
-  }
-  if (!call.request.getSecond() === 0) {
-    console.error('could not add, `Second` has not been provided');
-  }
+	// Validate input
+	if (call.request.getFirst() === 0) {
+		console.error("could not add, `First` has not been provided");
+	}
+	if (!call.request.getSecond() === 0) {
+		console.error("could not add, `Second` has not been provided");
+	}
 
-  // Log progress
-  console.log(
-    'adding',
-    call.request.getFirst(),
-    'to',
-    call.request.getSecond(),
-  );
+	// Log progress
+	console.log(
+		"adding",
+		call.request.getFirst(),
+		"to",
+		call.request.getSecond()
+	);
 
-  // Return added numbers
-  const reply = new MathAddReply();
-  reply.setResult(call.request.getFirst() + call.request.getSecond());
-  callback(null, reply);
+	// Return added numbers
+	const reply = new MathAddReply();
+	reply.setResult(call.request.getFirst() + call.request.getSecond());
+	callback(null, reply);
 };
 
 /**
  * Subtract two numbers
  */
 module.exports.subtract = (call, callback) => {
-  // Validate input
-  if (call.request.getFirst() === 0) {
-    console.error('could not subtract, `First` has not been provided');
-  }
-  if (!call.request.getSecond() === 0) {
-    console.error('could not subtract, `Second` has not been provided');
-  }
+	// Validate input
+	if (call.request.getFirst() === 0) {
+		console.error(
+			"could not subtract, `First` has not been provided"
+		);
+	}
+	if (!call.request.getSecond() === 0) {
+		console.error(
+			"could not subtract, `Second` has not been provided"
+		);
+	}
 
-  // Log progress
-  console.log(
-    'subtracting',
-    call.request.getSecond(),
-    'from',
-    call.request.getFirst(),
-  );
+	// Log progress
+	console.log(
+		"subtracting",
+		call.request.getSecond(),
+		"from",
+		call.request.getFirst()
+	);
 
-  // Return subtracted numbers
-  const reply = new MathAddReply();
-  reply.setResult(call.request.getSecond() + call.request.getFirst());
-  callback(null, reply);
+	// Return subtracted numbers
+	const reply = new MathAddReply();
+	reply.setResult(call.request.getSecond() + call.request.getFirst());
+	callback(null, reply);
 };
